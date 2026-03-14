134f7809fab6181dc3b8cc67363926083586d55a
Arel2014 <arelgumuskaya@gmail.com>
2026-03-14 09:14:54 +0300

    Update ModWorldGen.java

--- src/main/java/com/superhero/mod/worldgen/ModWorldGen.java
+++ src/main/java/com/superhero/mod/worldgen/ModWorldGen.java
@@ -1,7 +1,6 @@
-,package com.superhero.mod.worldgen;
+package com.superhero.mod.worldgen;
 
 public class ModWorldGen {
     public static void register() {
-        // World generation disabled for now
     }
 }
