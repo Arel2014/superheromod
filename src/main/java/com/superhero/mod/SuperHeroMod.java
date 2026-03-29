97c63d6ec59700c25d650a18ff1f1bccda4a9980
Arel2014 <arelgumuskaya@gmail.com>
2026-03-29 09:18:04 +0300

    Update SuperHeroMod.java

--- src/main/java/com/superhero/mod/SuperHeroMod.java
+++ src/main/java/com/superhero/mod/SuperHeroMod.java
@@ -1,6 +1,7 @@
 package com.superhero.mod;
 
 import com.superhero.mod.registry.ModItems;
+import com.superhero.mod.world.ModStructures;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,16 +10,17 @@ public class SuperHeroMod implements ModInitializer {
     public static final String MOD_ID = "superheromod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Senin istediğin mod ayarları (Taramalı mı Sniper mı?)
+    // Senin Ironman Menü Ayarların
     public static boolean isSniperMode = false; 
     public static boolean autoAttackEnabled = true;
     public static float flightSpeed = 1.0f;
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Süper Kahraman Modu Yükleniyor... Ironman Sistemleri Çevrimiçi!");
+        LOGGER.info("Süper Kahraman Modu Yükleniyor... Ironman ve Spiderman sistemleri hazır!");
         
-        // Eşyaları ve Zırhları Kaydet
-        ModItems.registerItems();
+        // --- TÜM SİSTEMLERİ BAŞLAT ---
+        ModItems.registerItems();    // Zırhları ve Eşyaları Tanıt
+        ModStructures.register();   // Villayı Dünyaya Ekle
     }
 }
