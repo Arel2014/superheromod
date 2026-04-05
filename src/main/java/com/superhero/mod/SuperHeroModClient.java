7b844e6739e1bc8ca1c83c1b83114d2c946f0219
Arel2014 <arelgumuskaya@gmail.com>
2026-04-05 08:55:31 +0300

    Update SuperHeroModClient.java

--- src/main/java/com/superhero/mod/SuperHeroModClient.java
+++ src/main/java/com/superhero/mod/SuperHeroModClient.java
@@ -7,11 +7,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 public class SuperheroModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // JARVIS ekranını (HUD) oyuna kaydediyoruz
-        // Bu kod, oyuncu Iron Man kaskını taktığında ekranı çizer
+        // JARVIS sistemini başlatıyoruz
         HudRenderCallback.EVENT.register(new JarvisHudOverlay());
-        
-        // Buraya gelecekte Spider-Man ağ fırlatma sesleri 
-        // veya diğer görsel efektleri de ekleyebiliriz!
     }
 }
